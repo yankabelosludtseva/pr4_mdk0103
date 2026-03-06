3dc6f56794675c6c760c3b49d082679bf6cce692
Shahzod0079 <toshmatovshahzod69@gmail.com>
2026-03-06 14:24:25 +0500

    Разработан базовый класс выполнения запросов. Белослудцева

---
+++ app/src/main/java/com/example/singin_belosludtseva/domains/apis/MyAsyncTask.java
@@ -0,0 +1,37 @@
+package com.example.singin_belosludtseva.domains.apis;
+
+import android.os.AsyncTask;
+
+import com.example.singin_belosludtseva.datas.CheckInternet;
+import com.example.singin_belosludtseva.domains.callbacks.MyResponseCallback;
+
+public class MyAsyncTask extends AsyncTask<Void, Void, String> {
+
+    protected MyResponseCallback callback;
+
+    protected CheckInternet checkInternet;
+
+    public MyAsyncTask(CheckInternet checkInternet, MyResponseCallback callback) {
+        this.checkInternet = checkInternet;
+        this.callback = callback;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        return "";
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        if(callback != null) {
+
+            if(result != null && !result.startsWith("Error")) {
+                callback.onComplete(result);
+            } else {
+                callback.onError(result);
+            }
+        }
+    }
+}
