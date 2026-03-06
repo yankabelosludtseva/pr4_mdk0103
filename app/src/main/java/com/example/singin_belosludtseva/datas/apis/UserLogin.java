73cab5579d6f87456dd116a0d9dea298fa6dc987
Shahzod0079 <toshmatovshahzod69@gmail.com>
2026-03-06 14:32:06 +0500

    Разработан класс авторизации. Белослудцева

---
+++ app/src/main/java/com/example/singin_belosludtseva/datas/apis/UserLogin.java
@@ -0,0 +1,47 @@
+package com.example.singin_belosludtseva.datas.apis;
+import com.example.singin_belosludtseva.datas.CheckInternet;
+import com.example.singin_belosludtseva.domains.apis.MyAsyncTask;
+import com.example.singin_belosludtseva.domains.callbacks.MyResponseCallback;
+import com.example.singin_belosludtseva.domains.models.User;
+import com.google.gson.GsonBuilder;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
+public class UserLogin extends MyAsyncTask {
+
+    private User user;
+
+    public UserLogin(User user, CheckInternet checkInternet, MyResponseCallback callback) {
+        super(checkInternet, callback);
+        this.user = user;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        if(!checkInternet.isWiFiConnection() && !checkInternet.isMobileConnection()) {
+            return "Error: no internet connection";
+        }
+
+        String rawData = new GsonBuilder().create().toJson(this.user);
+
+        try {
+            Connection.Response response = Jsoup.connect("http://10.111.20.114:5000/api/user/login")
+                    .ignoreContentType(true)
+                    .ignoreHttpErrors(true)
+                    .method(Connection.Method.POST)
+                    .header("Content-Type", "application/json")
+                    .requestBody(rawData)
+                    .execute();
+
+            return response.statusCode() == 200
+                    ? response.body()
+                    : "Error: " + response.body();
+
+        } catch (IOException e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/singin_belosludtseva/domains/models/User.java
@@ -0,0 +1,13 @@
+package com.example.singin_belosludtseva.domains.models;
+
+public class User {
+    public Integer id;
+    public String email;
+    public String password;
+    public String firstname;
+    public String lastname;
+    public String surname;
+    public String dateOfBirth;
+    public Integer gender;
+    public String token;
+}
